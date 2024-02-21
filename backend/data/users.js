11fe746331d0be6609d0175b2ed69a0529512853
datz <datblu2003@gmail.com>
2024-02-21 23:52:04 +0700

    add

---
+++ backend/data/users.js
@@ -0,0 +1,24 @@
+import bcrypt from 'bcryptjs';
+
+const users = [
+	{
+		name: 'Admin',
+		email: 'admin@email.com',
+		password: bcrypt.hashSync('1234', 10),
+		isAdmin: true,
+	},
+	{
+		name: 'Ha Nhat Huy',
+		email: 'huybucu@email.com',
+		password: bcrypt.hashSync('1234', 10),
+		isAdmin: false,
+	},
+	{
+		name: 'Tom Riu',
+		email: 'tomriu@email.com',
+		password: bcrypt.hashSync('1234', 10),
+		isAdmin: false,
+	},
+];
+
+export default users;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import {
 	RouterProvider,
 } from 'react-router-dom';
 
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import './index.css';
