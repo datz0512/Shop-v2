0d8ce3dcf1ad20eadac84d0d31a15a0a503bdfa8
datz <datblu2003@gmail.com>
2024-02-23 22:44:36 +0700

    add

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { errorHandler, notFound } from './middlewares/errorMiddleware.js';
 
 const port = process.env.PORT || 8080;
 
@@ -15,6 +17,9 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
 	console.log(`Server is running on  port ${port}`);
