6f12f67f7cea0864d69799044ec7c33ab1363aee
datz <datblu2003@gmail.com>
2024-02-28 00:05:25 +0700

    add backend authentication

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,19 +4,19 @@ const users = [
 	{
 		name: 'Admin',
 		email: 'admin@email.com',
-		password: bcrypt.hashSync('1234', 10),
+		password: bcrypt.hashSync('12345', 10),
 		isAdmin: true,
 	},
 	{
 		name: 'Ha Nhat Huy',
 		email: 'huybucu@email.com',
-		password: bcrypt.hashSync('1234', 10),
+		password: bcrypt.hashSync('12345', 10),
 		isAdmin: false,
 	},
 	{
 		name: 'Tom Riu',
 		email: 'tomriu@email.com',
-		password: bcrypt.hashSync('1234', 10),
+		password: bcrypt.hashSync('12345', 10),
 		isAdmin: false,
 	},
 ];
